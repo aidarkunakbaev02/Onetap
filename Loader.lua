fad07417aec2458cdb583b649922587f383376b9
SanCheats <164499956+aidarkunakbaev02@users.noreply.github.com>
2024-11-04 11:29:12 +0500

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aidarkunakbaev02/Onetap/refs/heads/main/Protected_2856579838612927.txt"))()
+if game.PlaceId == 3527629287 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/aidarkunakbaev02/Onetap/refs/heads/main/obf_48bF2DCbzzIMS2ch62L6ro6414oveFoYy9BPuP52njg8041gAHR3894y0r037Rc3.lua"))()
